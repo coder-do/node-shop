535fc450b6e5831f0216e9b66aef474ae55cfc3a
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-01-28 17:39:27 +0400

    fixes

--- models/product.js
+++ models/product.js
@@ -1,6 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'products.json'
+  );
+  
+const getProducts = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if (err) {
+            cb([]);
+        } else {
+            cb(JSON.parse(fileContent));
+        }
+    });
+};
+  
+
 module.exports = class Product {
     constructor(title, image, description, price) {
         this.title = title;
@@ -10,26 +27,23 @@ module.exports = class Product {
     }
 
     save() {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'product.json');
-        fs.readFile(p, (err, data) => {
-            let products = [];
-            if(!err) {
-                products = JSON.parse(data);
-            }
+        this.id = '_' + Math.random().toString(36).substr(2, 9);
+        getProducts(products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), err => {
-                console.log(err);
+              console.log(err);
             });
-        })
+        });      
     }
 
     static fetchAll(cb) {
-        const p = path.join(path.dirname(process.mainModule.filename), 'data', 'product.json');
-        fs.readFile(p, (err, data) => {
-            if(err) {
-                cb([]);
-            }
-            cb(JSON.parse(data));
+        getProducts(cb);
+    }
+
+    static findById(id, cb) {
+        getProducts(prod => {
+            const product = prod.find(p => p.id === id);
+            cb(product);
         })
     }
 }
