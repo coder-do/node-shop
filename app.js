143b9981bed6042e91218c7bd4cb971b6816214b
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-02-06 18:39:21 +0400

    hide db link

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const parser = require('body-parser');
+// dotenv
+require('dotenv').config();
 // db
 const { db } = require('./utils/db');
 // express
--- utils/db.js
+++ utils/db.js
@@ -5,7 +5,7 @@ const client = mongodb.MongoClient;
 let _db;
 
 const db = (cb) => {
-    client.connect('mongodb+srv://coder-do:coder-do@cluster0.rwygg.mongodb.net/node-shop?retryWrites=true&w=majority')
+    client.connect(process.env.DB_HOST)
         .then(res => {
             _db = res.db();
             cb();
