053ba4f9d3cc2330f141ac0f019e3b9f3bb447ac
Meruzh Kiloyan <meruzh.kiloyan@mail.ru>
2021-02-13 18:48:00 +0400

    validation

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const { getAuth, postAuth, postLogout, getSignup, postSignup } = require('../controllers/auth');
 
+const { check, body } = require('express-validator/check');
+
 const authRouter = express.Router();
 
 authRouter.get('/login', getAuth);
@@ -10,7 +12,10 @@ authRouter.post('/login', postAuth);
 
 authRouter.get('/signup', getSignup);
 
-authRouter.post('/signup', postSignup);
+authRouter.post('/signup', check('email').isEmail().withMessage('Enter a valid email'),
+    body('password', 'Password should be at least 6 charachters(text and number only)').isLength({ min: 6 })
+        .isAlphanumeric(),
+    postSignup);
 
 authRouter.post('/logout', postLogout);
 
